9d5c2ab1507a550fea211ed01c9600e85d694cab
ievavaiti <146743152+ieva-gsmd@users.noreply.github.com>
2024-03-14 22:30:15 +0000

    init

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,19 +1,26 @@
+//bool to track start/pause status
 let isPlaying = false;
 
+//yourplayer
 let player;
 
+//the text variable
 let intro;
 
+//your actual text
 let startText = "press anywhere to start audio"
 let stopText = "press anywhere to stop audio"
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  //so the text is in the middle
   rectMode(CENTER);
   
+  //default text
   intro = startText;
-  
+
+  //create new audio player, set it to loop
   player = new Tone.Player("samples/1.mp3").toDestination();
   player.loop = true;
   player.volume.value = -6;
@@ -22,16 +29,20 @@ function setup() {
 function draw() {
   background(220);
 
+  //map mouseX position to -20 to 0dB
   let playerVolume = map(mouseX, 0, width, -20, 0);
 
+  //set those mapped values to the audio player volume
   player.volume.value = playerVolume;
  
+  //use user interface text
   text(intro, width/2, height/2);
   
 }
 
 
 function mousePressed() {
+  //simple switch bool to start/stop audio
   if (isPlaying) {
     player.stop();
     isPlaying = false;
